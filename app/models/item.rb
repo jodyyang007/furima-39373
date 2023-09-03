b53b3a2b213b33a38fa726df853d237896e50dd2
jodyyang007 <yangjuanjuanjody89@gmail.com>
2023-09-03 18:14:30 +0900

    価格は半角数値のみ保存可能であること。

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,8 +18,12 @@ class Item < ApplicationRecord
   validates :estimated_shipping_id, presence: { message: "can't be blank" }
   validates :comment, presence: { message: "can't be blank" }
   validates :user, presence: true
-  validates :price, presence: { message: "can't be blank" }
-
+  validates :price, presence: true, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 300,
+    less_than_or_equal_to: 9_999_999,
+    message: "is invalid. Input half-width characters or Price is out of setting range."
+}
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
