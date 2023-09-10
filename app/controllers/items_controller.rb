81b5f3fc81efb4236b37fead42533059153c59cf
jodyyang007 <yangjuanjuanjody89@gmail.com>
2023-09-10 14:40:00 +0900

    編集問題あるときにエラーメッセージが表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,20 @@ else
 end
 end
 
+def edit
+  @item = Item.find(params[:id])
+ end
+
+
+ def update
+       @item = Item.find(params[:id])
+   if  @item.update(item_params)
+    redirect_to item_path(@item)
+  else
+    render 'edit'
+  end
+end
+
 # def purchase
 
 #   self.sold_out = true
