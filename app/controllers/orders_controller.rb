9cf11ed04f0184179d166fc8f4cf8306c2c25e83
jodyyang007 <yangjuanjuanjody89@gmail.com>
2023-09-16 16:09:03 +0900

    購入入力フォームの保存の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,18 +6,20 @@ class OrdersController < ApplicationController
   end
 
     def create
+    binding.pry
+    @item = Item.find(params[:item_id])
     @order_shipping = OrderShipping.new(order_params)
     if @order_shipping.valid?
        @order_shipping.save
        return redirect_to root_path
     else
-      render :new
+      render :index
      end
     end
 
   private
 
   def order_params
-    params.require(:order_shipping).permit(:postal_code, :prefecture, :city, :street_address, :building_name, :phone_number).merge(item_id: @item.id)
+    params.require(:order_shipping).permit(:postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number).merge(item_id: @item.id, user_id: current_user.id)
   end
 end
--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -1,14 +1,20 @@
 class OrderShipping
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture, :city, :street_address, :building_name, :phone_number
+  attr_accessor :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :item_id, :user_id
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
-    validates :prefecture, numericality: { other_than: 0, message: "can't be blank" }
+    validates :prefecture_id, presence: { message: "can't be blank" }
+    validates :city
+    validates :street_address
+    validates :phone_number, format: { with: /\A[0-9]{11}\z/ }
+    validates :item_id
+    validates :user_id
 end
 
+
   def save
-    order = Order.create(item: item, user_id: user_id)
-    Address.create(postal_code: postal_code, prefecture: prefecture, city: city, street_address: street_address, building_name: building_name, phone_number:phone_number, order_id: order.id)
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street_address: street_address,building_name: building_name, phone_number:phone_number, order_id: order.id)
   end
 end
