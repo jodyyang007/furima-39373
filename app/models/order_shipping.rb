e2683904f6b5edfd2a64be4b7121c36348b754e2
jodyyang007 <yangjuanjuanjody89@gmail.com>
2023-09-19 22:01:33 +0900

    prefecture_idバリュエーション修正
    validates :prefecture_id, presence: { message: "can't be blank" }
    「---」に該当するidを弾くバリデーションを指定しましょう！

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -7,7 +7,7 @@ class OrderShipping
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
-    validates :prefecture_id, presence: { message: "can't be blank" }
+    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
     validates :city
     validates :street_address
     validates :phone_number, format: { with: /\A[0-9]{11}\z/ }
