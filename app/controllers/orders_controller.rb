43e5a0ea59bad0276b52a5f5128283215f755025
jodyyang007 <yangjuanjuanjody89@gmail.com>
2023-09-24 17:14:03 +0900

    本番環境確認修正
     カード情報を入力せずに購入ボタンを押すと、エラー画面に遷移してしまいます。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,6 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :find_item, only: [:index, :create,:show]
 
-  
   def index
     @order = Order.new
     @order_shipping = OrderShipping.new
@@ -14,40 +13,35 @@ end
     def create
     @order_shipping = OrderShipping.new(order_params)
     @order = Order.new(item_id: @item.id, user_id: current_user.id)
-    pay_item
+   
 
     if current_user == @item.user
       return redirect_to root_path
     elsif @order_shipping.valid? && @order.save
+      pay_item
       @item.mark_as_sold_out
       redirect_to root_path
     else
       render :index
     end
-  end
-    
-
-    def pay_item
-    
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    
-
-    amount = @item.price
-
-    Payjp::Charge.create(
-      amount: amount,  
-      card: order_params[:token],    
-      currency: 'jpy'                
-    )
     end
-  
-    def find_item
-      @item = Item.find(params[:item_id])
-    end
-
+    
     private
 
-  def order_params
-    params.require(:order_shipping).permit(:postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number).merge(item_id: @item.id, user_id: current_user.id, token: params[:token])
-  end
-end
+      def pay_item 
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],    
+        currency: 'jpy'                
+      )
+      end
+    
+      def find_item
+        @item = Item.find(params[:item_id])
+      end
+
+      def order_params
+      params.require(:order_shipping).permit(:postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number).merge(item_id: @item.id, user_id: current_user.id, token: params[:token])
+      end
+     end
--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -1,6 +1,6 @@
 class OrderShipping
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :item_id, :user_id,:token
+  attr_accessor :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :item_id, :user_id, :token
 
    with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
